9f162aba466bbce97850beb2d42a1aded1d3259a
Dariusz Buszman <dbuszman@hotmail.com>
2016-04-09 23:38:47 +0200

    Added static_pages, custom stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
   resources :flatmates
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
-
+  # root 'flatmates#index'
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
