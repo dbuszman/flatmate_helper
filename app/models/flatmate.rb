aec82234880fc9b24231c1d2a39c71da57fc752f
Dariusz Buszman <dbuszman@hotmail.com>
2016-04-10 19:54:01 +0200

    add validation

--- app/models/flatmate.rb
+++ app/models/flatmate.rb
@@ -1,4 +1,10 @@
 class Flatmate < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  validates :firstname, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :lastname, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :duties, presence: true
+  validates :charges, presence: true
+
 end
